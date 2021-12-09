fc1460ae7b79d914969f9df2dee42cd1805fa56c
Deepak Singh <d4singh@bravurasolutions.com>
2021-12-09 09:00:21 +0530

    added

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "first_bucket" {
+    type = string
+    default = "gcs-bucket-abc1"
+}
+variable "second_bucket" {
+    type = string
+    default = "first-gcs-bucket-abc44"
+}
+variable "third_bucket" {
+    type = string
+    default = "first-gcs-bucket-abc3"
+}
+variable "forth_bucket" {
+    type = string
+    default = "first-gcs-bucket-abc4"
+}
